7a497550caae4b10a54ae2a3a6d317179530735d
Sajal Chakraverty <ksajal6689@gmail.com>
2021-12-01 10:14:00 +0530

    change

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
 
-            callback(undefined, body.current.weather_descriptions[0]+" It Is currently " + body.current.temperature +" DEGREE it feels like "+ body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0]+" It Is currently " + body.current.temperature +" DEGREE fahrenheit it feels like "+ body.current.feelslike)
         }
     })
 }
